526477cf64c70372db63bf77e42a64e20b836d33
maxluchs <>
2021-03-05 21:26:13 +0100

    use combinators for err handling

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,5 @@
-use std::sync::{Arc, Mutex};
+use std::convert::Infallible;
+use std::sync::{Arc, Mutex, MutexGuard};
 use warp::Filter;
 use warp_ships::db_models::{Ship, DB};
 
@@ -9,23 +10,24 @@ async fn main() {
     let db: SharableDB = Arc::new(Mutex::new(DB::new(&db_url)));
 
     let root = warp::any();
-    let get_ships = root
+    let all_ships = root
         .and(warp::get())
-        .and(warp::any().map(move || {
-            let db_clone = db.clone();
-            db_clone
-        }))
-        .map(|db: SharableDB| {
-            let db = db.lock().unwrap();
-            let all_ships = db.list_ships(None);
-            return all_ships
-                .map(|all_ships| {
-                    return warp::reply::json::<Vec<Ship>>(&all_ships);
+        .and(warp::any().map(move || db.clone()))
+        .and_then(|db: SharableDB| async move {
+            db.lock()
+                .map_err(|_e| warp::reject::not_found())
+                .and_then(|db: MutexGuard<DB>| {
+                    db.list_ships(None).map_err(|_e| warp::reject::not_found())
                 })
-                .unwrap();
-            //.map_err(|_| warp::reject::reject());
+                .map(|all_ships: Vec<Ship>| warp::reply::json::<Vec<Ship>>(&all_ships))
+        })
+        .recover(|_e: warp::Rejection| async move {
+            Ok::<_, Infallible>(warp::reply::with_status(
+                "Error",
+                warp::http::StatusCode::INTERNAL_SERVER_ERROR,
+            ))
         });
-    let ships = warp::path("ships").and(get_ships);
+    let ships = warp::path("ships").and(all_ships);
     warp::serve(ships).run(([127, 0, 0, 1], 4000)).await;
 }
 
