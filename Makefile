6f6bd250421739eb94fcb242eb6fc2fd910f0654
maxluchs <>
2021-03-06 00:31:34 +0100

    added error handler recover

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ include: .env
 export
 
 watch:
-	RUST_LOG=debug cargo watch -w src -x "run --bin ship"
+	RUST_LOG=debug cargo watch -w src -x "lrun --bin ship"
 seed:
 	RUST_LOG=debug cargo run --bin seed
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,37 @@
+use serde::Serialize;
 use std::convert::Infallible;
 use std::sync::{Arc, Mutex, MutexGuard};
-use warp::Filter;
+use warp::reply::Json;
+use warp::{Filter, Rejection};
 use warp_ships::db_models::{Ship, DB};
+
+#[derive(Serialize, Debug, Clone)]
+struct ServiceError {
+    service_name: String,
+    error_message: String,
+}
+
+impl ServiceError {
+    fn new(service_name: String, error: String) -> ServiceError {
+        ServiceError {
+            service_name,
+            error_message: error.to_string(),
+        }
+    }
+}
+
+impl warp::reject::Reject for ServiceError {}
+
+async fn handle_rejection(rejection: Rejection) -> Result<Json, Infallible> {
+    let response = rejection
+        .find::<ServiceError>()
+        .map(|err| err.to_owned())
+        .unwrap_or(ServiceError {
+            service_name: "unknown".to_owned(),
+            error_message: "unknown error".to_owned(),
+        });
+    return Ok::<Json, Infallible>(warp::reply::json(&response));
+}
 
 #[tokio::main]
 async fn main() {
@@ -15,20 +45,22 @@ async fn main() {
         .and(warp::any().map(move || db.clone()))
         .and_then(|db: SharableDB| async move {
             db.lock()
-                .map_err(|_e| warp::reject::not_found())
+                .map_err(|e| {
+                    warp::reject::custom(ServiceError::new("db".to_owned(), format!("{}", e)))
+                })
                 .and_then(|db: MutexGuard<DB>| {
-                    db.list_ships(None).map_err(|_e| warp::reject::not_found())
+                    db.list_ships(None).map_err(|e| {
+                        warp::reject::custom(ServiceError::new(
+                            "list_ships".to_owned(),
+                            format!("{:?}", e),
+                        ))
+                    })
                 })
                 .map(|all_ships: Vec<Ship>| warp::reply::json::<Vec<Ship>>(&all_ships))
-        })
-        .recover(|_e: warp::Rejection| async move {
-            Ok::<_, Infallible>(warp::reply::with_status(
-                "Error",
-                warp::http::StatusCode::INTERNAL_SERVER_ERROR,
-            ))
         });
     let ships = warp::path("ships").and(all_ships);
-    warp::serve(ships).run(([127, 0, 0, 1], 4000)).await;
+    let root = warp::any().and(ships).recover(handle_rejection);
+    warp::serve(root).run(([127, 0, 0, 1], 4000)).await;
 }
 
 type SharableDB = Arc<Mutex<DB>>;
